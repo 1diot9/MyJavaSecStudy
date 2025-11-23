aba303a8d07f51faf0848fcf52c3b6323a155c21
1diot9 <2868944833@qq.com>
2025-11-23 13:55:36 +0800

    rename folder

--- hessian/HessianStudy/src/main/java/com/hessian/asServlet/Hello.java
+++
@@ -1,9 +0,0 @@
-package com.hessian.asServlet;
-
-import com.hessian.pojo.Person;
-
-public interface Hello {
-    public String sayHello(String name);
-
-    public Person changeAge(Person person, int age);
-}
